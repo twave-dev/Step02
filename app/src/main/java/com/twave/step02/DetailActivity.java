aca388f737b2be3fc2847997c511f457029314ee
lotbaselog <lotbaselog@gmail.com>
2017-05-16 13:07:47 +0900

    04-01-LaunchNewActivity

---
+++ app/src/main/java/com/twave/step02/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.twave.step02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/com/twave/step02/MainActivity.java
+++ app/src/main/java/com/twave/step02/MainActivity.java
@@ -1,6 +1,7 @@
 package com.twave.step02;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -79,6 +80,9 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
     public void onClick(String weatherForDay) {
         Context context = this;
         Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT).show();
+        Class destinationClass = DetailActivity.class;
+        Intent intentToStartDetailActivity = new Intent(context, destinationClass);
+        startActivity(intentToStartDetailActivity);
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
