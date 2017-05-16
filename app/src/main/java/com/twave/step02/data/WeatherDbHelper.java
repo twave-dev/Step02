e167bdf5c01460da522c03dacabbab2709bd69e8
lotbaselog <lotbaselog@gmail.com>
2017-05-16 19:42:57 +0900

    07-02-PreventInvalidInserts

--- app/src/main/java/com/twave/step02/data/WeatherDbHelper.java
+++ app/src/main/java/com/twave/step02/data/WeatherDbHelper.java
@@ -29,7 +29,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "weather.db";
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -44,24 +44,25 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
                 WeatherEntry._ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 
-                WeatherEntry.COLUMN_DATE       + " INTEGER, "                 +
+                WeatherEntry.COLUMN_DATE       + " INTEGER NOT NULL, "                 +
 
-                WeatherEntry.COLUMN_WEATHER_ID + " INTEGER, "                 +
+                WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, "                 +
 
-                WeatherEntry.COLUMN_MIN_TEMP   + " REAL, "                    +
-                WeatherEntry.COLUMN_MAX_TEMP   + " REAL, "                    +
+                WeatherEntry.COLUMN_MIN_TEMP   + " REAL NOT NULL, "                    +
+                WeatherEntry.COLUMN_MAX_TEMP   + " REAL NOT NULL, "                    +
 
-                WeatherEntry.COLUMN_HUMIDITY   + " REAL, "                    +
-                WeatherEntry.COLUMN_PRESSURE   + " REAL, "                    +
+                WeatherEntry.COLUMN_HUMIDITY   + " REAL NOT NULL, "                    +
+                WeatherEntry.COLUMN_PRESSURE   + " REAL NOT NULL, "                    +
 
-                WeatherEntry.COLUMN_WIND_SPEED + " REAL, "                    +
-                WeatherEntry.COLUMN_DEGREES    + " REAL" + ");";
+                WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, "                    +
+                WeatherEntry.COLUMN_DEGREES    + " REAL NOT NULL" + ");";
 
         sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
