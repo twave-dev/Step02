bdcb603118003b9e56d290d3ec36153cb119b88a
lotbaselog <lotbaselog@gmail.com>
2017-05-16 18:12:47 +0900

    06-02-SettingsFragment

---
+++ app/src/main/java/com/twave/step02/SettingsFragment.java
@@ -0,0 +1,75 @@
+package com.twave.step02;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String key = preference.getKey();
+        String stringValue = value.toString();
+
+        if(preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if(prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.pref_general);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = prefScreen.getSharedPreferences();
+        int count = prefScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if(!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if(preference != null) {
+            if(!(preference instanceof CheckBoxPreference)) {
+                setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+        }
+    }
+}
