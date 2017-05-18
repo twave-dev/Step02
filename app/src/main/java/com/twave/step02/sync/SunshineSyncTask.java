9b91e85efdf18297f6e24e4189f1a55fec1aa804
lotbaselog <lotbaselog@gmail.com>
2017-05-18 16:06:40 +0900

    10-04-Notifications

--- app/src/main/java/com/twave/step02/sync/SunshineSyncTask.java
+++ app/src/main/java/com/twave/step02/sync/SunshineSyncTask.java
@@ -18,9 +18,12 @@ package com.twave.step02.sync;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.text.format.DateUtils;
 
+import com.twave.step02.data.SunshinePreferences;
 import com.twave.step02.data.WeatherContract;
 import com.twave.step02.utilities.NetworkUtils;
+import com.twave.step02.utilities.NotificationUtils;
 import com.twave.step02.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
@@ -40,47 +43,40 @@ public class SunshineSyncTask {
      */
     synchronized public static void syncWeather(Context context) {
 
-//      COMPLETED (3) Within syncWeather, fetch new weather data
 
         try {
-            /*
-             * The getUrl method will return the URL that we need to get the forecast JSON for the
-             * weather. It will decide whether to create a URL based off of the latitude and
-             * longitude or off of a simple location as a String.
-             */
             URL weatherRequestUrl = NetworkUtils.getUrl(context);
 
-            /* Use the URL to retrieve the JSON */
             String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
 
-            /* Parse the JSON into a list of weather values */
             ContentValues[] weatherValues = OpenWeatherJsonUtils
                     .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
 
-            /*
-             * In cases where our JSON contained an error code, getWeatherContentValuesFromJson
-             * would have returned null. We need to check for those cases here to prevent any
-             * NullPointerExceptions being thrown. We also have no reason to insert fresh data if
-             * there isn't any to insert.
-             */
             if (weatherValues != null && weatherValues.length != 0) {
-                /* Get a handle on the ContentResolver to delete and insert data */
                 ContentResolver sunshineContentResolver = context.getContentResolver();
 
-//              COMPLETED (4) If we have valid results, delete the old data and insert the new
-                /* Delete old weather data because we don't need to keep multiple days' data */
                 sunshineContentResolver.delete(
                         WeatherContract.WeatherEntry.CONTENT_URI,
                         null,
                         null);
 
-                /* Insert our new weather data into Sunshine's ContentProvider */
                 sunshineContentResolver.bulkInsert(
                         WeatherContract.WeatherEntry.CONTENT_URI,
                         weatherValues);
+
+                boolean notificationsEnabled = SunshinePreferences.areNotificationsEnabled(context);
+                long timeSinceLastNotification = SunshinePreferences.getEllapsedTimeSinceLastNotification(context);
+                boolean oneDayPassedSinceLastNotification = false;
+
+                if (timeSinceLastNotification >= DateUtils.DAY_IN_MILLIS) {
+                    oneDayPassedSinceLastNotification = true;
+                }
+
+                if (notificationsEnabled && oneDayPassedSinceLastNotification) {
+                    NotificationUtils.notifyUserOfNewWeather(context);
+                }
             }
 
-            /* If the code reaches this point, we have successfully performed our sync */
 
         } catch (Exception e) {
             /* Server probably invalid */
